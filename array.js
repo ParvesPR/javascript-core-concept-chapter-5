e7c5e0afa1030ee155aa692b9be2acde02df6153
ParvesPR <parves2585@gmail.com>
2024-02-28 23:47:21 +0800

    array declaration

--- array.js
+++ array.js
@@ -1,1 +1,12 @@
-var arr = [1, 2, 3, 4, 5]
+// Method 1
+var arr = [1, 2, 3, 4, 5];
+arr[5] = 6; /* Push a data using index position */
+arr[2] = 33;
+console.log(arr[0]);/* Find value using index position */
+console.log(arr[3]);
+console.log(arr);
+console.log(arr.length);
+
+// Method 2
+var arr2 = Array(1, 2, 3);
+console.log(arr2);
