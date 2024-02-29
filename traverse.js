706a900bd97b4fcec8f69dbd86bae8366f857cb4
ParvesPR <parves2585@gmail.com>
2024-02-29 13:58:27 +0800

    odd number summation in an array

--- traverse.js
+++ traverse.js
@@ -1,8 +1,25 @@
 // Traverse an array
-var arr = [1, 2, 45, 65, 7, 89];
-var str = -1
+var arr = [1, 2, 46, 65, 8, 89];
+var arr2 = [1, 0, 40, 30, 20, 10];
 
 for (var x = 0; x < arr.length; x++) {
-    console.log(arr[x]);
+    arr[x] = arr[x] + 1; /* 1 will be added to each number */
+    // console.log(arr[x]);
 }
 
+// Summation an array
+var sum = 0;
+for (var x = 0; x < arr2.length; x++) {
+    sum += arr2[x];
+}
+// console.log(sum);
+
+// Summation ODD number
+var sum = 0;
+for (var x = 0; x < arr.length; x++) {
+    if (arr[x] % 2 === 1) {
+        console.log(arr[x]);
+        sum += arr[x];
+    }
+}
+console.log('result = ' + sum);
