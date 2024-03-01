90987fc3217131c01a80a740e9e65be234b71e26
ParvesPR <parves2585@gmail.com>
2024-03-01 13:00:47 +0800

    array insert-remove method

---
+++ insert-remove.js
@@ -0,0 +1,11 @@
+// Insert & Remove Element
+var arr = [1, 2, 3, 4, 5, 6, 7];
+// Insert 9 in the last index
+arr.push(9);
+// Remove the last element
+arr.pop();
+// Remove the first element
+arr.shift()
+// Insert element at the start of the array
+arr.unshift(10)
+console.log(arr);
