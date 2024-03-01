4f6e52eed1195194065f7ec974a72f3cfd485eac
ParvesPR <parves2585@gmail.com>
2024-03-01 13:22:36 +0800

    delete element and update element in an array

--- insert-remove.js
+++ insert-remove.js
@@ -1,13 +1,16 @@
 // Insert & Remove Element
 var arr = [1, 2, 3, 4, 5, 6, 7];
-// Insert 9 in the last index
-arr.push(9);
+// Remove the first element
+arr.shift()
 // Remove the last element
 arr.pop();
-// Remove the first element
-// arr.shift()
+// Insert 9 in the last index
+arr.push(9);
 // Insert element at the start of the array
 arr.unshift(10)
 // Index position, delete-count and insert new element
-arr.splice(4, 1, 9, 11)
+// arr.splice(4, 1, 9, 11);
+// Delete element insert according to index number
+arr.splice(3, 1);
+arr[3] = 40;
 console.log(arr);
